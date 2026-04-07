f6b8c9987c7adc30b11a2e238b0a285bf10fb42f
hpeerage <111835551+hpeerage@users.noreply.github.com>
2026-04-07 19:01:54 +0900

    Refactor dotenv import and configuration

--- src/lib/turso.ts
+++ src/lib/turso.ts
@@ -1,16 +1,13 @@
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./db/schema";
-import * as dotenv from "dotenv";
-
-dotenv.config();
 
 const url = process.env.TURSO_DATABASE_URL!;
 const authToken = process.env.TURSO_AUTH_TOKEN!;
 
 export const client = createClient({
-  url: url,
-  authToken: authToken,
+    url: url,
+    authToken: authToken,
 });
 
 export const db = drizzle(client, { schema });
